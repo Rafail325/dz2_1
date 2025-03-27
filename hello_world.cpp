a4febeca0360b6c91509e50cb17562bd63d7e791
Rafail325 <bikbaev-1981@bk.ru>
2025-03-27 17:40:02 -0400

    Better cpp file

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-using namespace std;  // Плохой стиль!
 
 int main() {
-    cout << "Hello world!" << endl;
-    return 0;
+	std::string name;
+	std::cout << "Enter your name: " << std::endl;
+	std::cin >> name;
+	std::cout << "Hello world from " << name << std::endl;
+	return 0;
 }
-
