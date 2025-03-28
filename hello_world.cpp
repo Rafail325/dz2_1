9ae4b9b63fe94a7584d7f0c4ac672f30692076f8
Rafail325 <bikbaev-1981@bk.ru>
2025-03-28 06:20:03 -0400

    Comments on English

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,13 @@
+
 #include <iostream>
 
-int //тело основной функции
+int //body of main fucntion
 main()
 {
-  std::string name; //переменная имени
+  std::string name; //creating the name
   std::cout << "Enter your name: " << std::endl;
-  std::cin >> name;//Ввод переменной name
+  std::cin >> name;//entering the meaning of name
   std::cout << "Hello world from " << name << std::endl;
   return 0;
 }
+// This program asks for user's name
